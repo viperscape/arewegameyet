21627b9a9a4714ef9b62ff781633fd2248ff6163
viperscape <chrisfgill@gmail.com>
2017-08-02 05:51:15 -0400

    helper fn

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #[macro_use] extern crate serde_derive;
 
+use std::collections::HashMap;
+
 mod config;
 use config::Config;
 
@@ -15,12 +17,20 @@ use render::Render;
 fn main () {
     let cats = Categories::default();
 
-    for n in cats.scripting {
-        let url = "https://crates.io/api/v1/crates/".to_owned() + &n;
-        let c = Crate::from_url(&url);
-        println!("{:?}",c);
-    }
+    let scripting = collect(&cats.scripting);
+    println!("{:?}",scripting);
 
     let r = Render::from_file("./views/crates.html").expect("Cannot parse template view");
-    let _b = r.render();
+    let mut data = HashMap::new();
+    data.insert("crates", scripting);
+    let b = r.render(data);
+}
+
+/// Parse and collect crate info
+fn collect (v: &[String]) -> Vec<Crate> {
+    v.iter().map(|ref n| {
+            let url = "https://crates.io/api/v1/crates/".to_owned() + &n;
+            let c = Crate::from_url(&url);
+            c
+    }).filter(|n| n.is_some()).map(|n| n.unwrap()).collect()
 }
